3fa530b96a8fcfa1939cca07d9be5b36b58d4a66
pierrdu <pierre@duhem.com>
2016-05-16 16:16:27 +0200

    1.0.4

--- event/listener.php
+++ event/listener.php
@@ -20,20 +20,36 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class listener implements EventSubscriberInterface
 {
 
+	/* @var \phpbb\template\template */
+	protected $template;
+
+	public function __construct(\phpbb\template\template $template)
+	{
+		$this->template = $template;
+	}
+
 	static public function getSubscribedEvents ()
 	{
 	return array(
-		'core.posting_modify_template_vars'	=>	'delete_re',
+		'core.posting_modify_template_vars' => 'delete_re',
+		'core.viewtopic_modify_page_title' => 'delete_re_2',
 	);
 	}
 
 	public function delete_re ($event)
 	{
 		$page_data = $event['page_data'];
-		$titre = $page_data['SUBJECT'];
+		$titre = $page_data['subject'];
 		$titre = preg_replace('/^Re: /', '', $titre);
-		$page_data['SUBJECT'] = $titre;
+		$page_data['subject'] = $titre;
 		$event['page_data'] = $page_data;
 	}
+
+	public function delete_re_2 ($event)
+	{
+		$topic_data = $event['topic_data'];
+		$this->template->assign_var ('SUBJECT', censor_text($topic_data['topic_title']));
+	}
+
 
 }
